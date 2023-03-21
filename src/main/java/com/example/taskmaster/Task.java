c5e368c2dd75f192c5e65dc416071c356ff0b6a5
David Brandstetter <dbrandstetter0@gmail.com>
2023-03-21 12:26:25 +0100

    added new Task class

--- src/main/java/com/example/taskmaster/Task.java
+++ src/main/java/com/example/taskmaster/Task.java
@@ -5,5 +5,42 @@ public class Task {
     private String deadline;
     private String info;
 
+    public Task(String title, String deadline, String info) {
+        this.title = title;
+        this.deadline = deadline;
+        this.info = info;
+    }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(String deadline) {
+        this.deadline = deadline;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "title='" + title + '\'' +
+                ", deadline='" + deadline + '\'' +
+                ", info='" + info + '\'' +
+                '}';
+    }
 }
