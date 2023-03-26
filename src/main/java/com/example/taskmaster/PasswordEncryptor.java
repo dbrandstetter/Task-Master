2e692eb5f4dc27d2e767eb4424c00dad66b011c4
David <dbrandstetter0@gmail.com>
2023-03-26 13:20:11 +0200

    reformatted code

--- src/main/java/com/example/taskmaster/PasswordEncryptor.java
+++ src/main/java/com/example/taskmaster/PasswordEncryptor.java
@@ -13,8 +13,8 @@ public class PasswordEncryptor {
 
 		StringBuilder sb = new StringBuilder();
 
-		for (int i = 0; i < bytes.length; i++) {
-			sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+		for (byte octet : bytes) {
+			sb.append(Integer.toString((octet & 0xff) + 0x100, 16).substring(1));
 		}
 
 		return sb.toString();
